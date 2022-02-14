ec6a418e61c232770a27789ab2dfae5c11700126
Han Wang <hyw2@cornell.edu>
2022-02-14 00:42:06 -0800

    Add homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,138 +1,95 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
 import {
-  MenuIcon,
-  XIcon,
-} from '@heroicons/react/outline'
+  SearchIcon,
+  SunIcon,
+  MoonIcon,
+  ArrowRightIcon
+} from '@heroicons/react/solid';
 
-const navigation = [
-  { name: 'Introduction', href: '#', current: true },
-  { name: 'Including Stripe.js', href: '#', current: false },
+const menuSections = [
+  {
+    title: 'Setting Up',
+    pages: [
+      { name: 'Introduction', href: '#', current: true },
+      { name: 'Quickstart', href: '#', current: false },
+      { name: 'How to start', href: '#', current: false },
+    ]
+  }
 ]
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
     <>
       <div>
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="fixed inset-0 flex z-40 md:hidden" onClose={setSidebarOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                    </button>
-                  </div>
-                </Transition.Child>
-                <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                  <div className="flex-shrink-0 flex items-center px-4">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <nav className="mt-5 px-2 space-y-1">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                          'group flex items-center px-2 py-2 text-base font-medium rounded-md'
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </nav>
-                </div>
-                <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-                  <a href="#" className="flex-shrink-0 group block">
-                    <div className="flex items-center">
-                      <div>
-                        <img
-                          className="inline-block h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div className="ml-3">
-                        <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                        <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </Transition.Child>
-            <div className="flex-shrink-0 w-14">{/* Force sidebar to shrink to fit close icon */}</div>
-          </Dialog>
-        </Transition.Root>
-
         {/* Static sidebar for desktop */}
         <div className="hidden md:flex md:w-56 md:flex-col md:fixed md:inset-y-0">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
-            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex items-center flex-shrink-0 px-4">
+            <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+              <div className="w-full flex items-center flex-shrink-0 px-4">
+                <div className="flex-1">
                 <img
-                  className="h-8 w-auto"
+                  className="h-6 w-auto"
                   src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
                   alt="Workflow"
                 />
+                </div>
+                <div className="flex-1 flex items-center justify-end">
+                <Switch
+                  checked={isDarkMode}
+                  onChange={setIsDarkMode}
+                  className="flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none"
+                >
+                  <span className="sr-only">Use setting</span>
+                  <span aria-hidden="true" className="pointer-events-none absolute bg-white w-full h-full rounded-md" />
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      isDarkMode ? 'bg-indigo-600' : 'bg-gray-200',
+                      'pointer-events-none absolute h-3 w-6 mx-auto rounded-full transition-colors ease-in-out duration-200'
+                    )}
+                  />
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      isDarkMode ? 'translate-x-5' : 'translate-x-0',
+                      'flex items-center justify-center pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white transform ring-0 transition-transform ease-in-out duration-200'
+                    )}
+                  >
+                    {
+                      isDarkMode ? 
+                        <MoonIcon
+                          className="h-4 w-4 text-gray-500"
+                        />
+                        :
+                        <SunIcon
+                          className="h-4 w-4 text-yellow-400"
+                        />
+                    }
+                  </span>
+                </Switch>
+                </div>
               </div>
-              <div className="mt-4">
+              <div className="mt-2">
                 <button type="button" className="px-3 mt-1 relative flex items-center">
+                  <div className="absolute left-5">
+                    <SearchIcon className="h-4 w-4 text-gray-500" />
+                  </div>
                   <input
                     type="text"
                     name="search"
                     id="search"
                     placeholder="Find anything"
                     disabled
-                    className="cursor-pointer px-2 py-1 shadow-sm bg-gray-100 block w-full pr-12 sm:text-sm border border-gray-200 rounded-md focus:outline-none"
+                    className="cursor-pointer py-1 pl-7 pr-12 bg-gray-50 block w-full sm:text-vs border font-light border-gray-200 rounded-md focus:outline-none hover:border-gray-300"
                   />
                   <div className="absolute inset-y-0 right-0 flex py-1 pr-4">
                     <kbd className="inline-flex items-center border border-gray-200 rounded px-1 text-sm font-sans font-light text-gray-400">
@@ -142,20 +99,27 @@ export default function Example() {
                 </button>
               </div>
               <nav className="mt-5 flex-1 pr-3 bg-white">
-              <div className="mb-1 px-4 uppercase text-xs font-medium">
-                Setting up
-              </div>
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      item.current ? 'bg-green-50 text-green-600' : 'text-gray-500 hover:bg-gray-50 hover:text-gray-900',
-                      'group flex items-center pl-4 py-1.5 text-vs font-medium rounded-r-lg'
+                {menuSections.map((section) => (
+                  <>
+                  <div className="mb-1 px-4 uppercase text-xs font-medium text-gray-700">
+                    {section.title}
+                  </div>
+                  <div className="mt-2">
+                  {
+                    section.pages.map((page) =>
+                      <a
+                        key={page.name}
+                        href={page.href}
+                        className={classNames(
+                          page.current ? 'bg-green-50 text-green-600' : 'text-gray-500 hover:bg-gray-50 hover:text-gray-900',
+                          'group flex items-center pl-4 py-1 text-vs rounded-r-lg'
+                        )}
+                      >
+                        {page.name}
+                      </a>
                     )}
-                  >
-                    {item.name}
-                  </a>
+                  </div>
+                  </>
                 ))}
               </nav>
             </div>
@@ -178,28 +142,47 @@ export default function Example() {
             </div>
           </div>
         </div>
-        <div className="md:pl-64 flex flex-col flex-1">
+        <div className="md:pl-56 flex flex-col flex-1">
           <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
-            <button
-              type="button"
-              className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-              onClick={() => setSidebarOpen(true)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
+            Menu
           </div>
           <main className="flex-1">
-            <div className="py-6">
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            <nav className="flex flex-row justify-end space-x-6 px-16 py-5 text-sm">
+            <div className="flex items-center text-green-700">
+                <h1 className="mr-1">
+                  About
+                </h1>
               </div>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                {/* Replace with your content */}
-                <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            <div className="flex items-center text-green-700">
+                <h1 className="mr-1">
+                  Docs
+                </h1>
+              </div>
+              <div className="flex items-center text-green-700">
+                <h1 className="mr-1">
+                  Dashboard
+                </h1>
+                <ArrowRightIcon className="h-4 w-4" />
+              </div>
+            </nav>
+            <div className="w-full py-20 border-b border-gray-200 flex justify-center">
+              <div className="max-w-[68rem]">
+                <h1 className="text-gray-800 text-2xl tracking-tight">Stripe.js reference</h1>
+                <div className="mt-5 flex text-gray-700 space-x-14 leading-relaxed">
+                <div className="flex-1">
+                  <p>
+                    This reference documents every object and method available in Stripe’s browser-side JavaScript library, Stripe.js. Use our React <a className="text-green-700">Stripe.js reference</a> if you want to add Elements to your React based app.
+                  </p>
+                  <br />
+                  <p>
+                    You can use Stripe.js APIs to tokenize customer information, collect sensitive payment details using customizable Stripe Elements, and accept payments with browser payment APIs like Apple Pay and the Payment Request API.
+                  </p>
                 </div>
-                {/* /End replace */}
+                <div className="flex-1">
+                  <h2 className="text-gray-800 uppercase font-medium">Looking for a step by step guide?</h2>
+                  <p className="mt-2 text-gray-500">Learn to accept a payment or use the Payment Request Button.</p>
+                </div>
+                </div>
               </div>
             </div>
           </main>
